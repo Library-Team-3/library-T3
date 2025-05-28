4d182637d97afd62eb5ae60df45e94afaa82d2da
VitaPoperechna <vitapoperechna@gmal.com>
2025-05-28 14:31:59 +0200

    feat: added class genre with attributes, getters/setters and methods

---
+++ src/main/java/org/LT3/model/Genre.java
@@ -0,0 +1,32 @@
+package org.LT3.model;
+
+public class Genre {
+    Long id;
+    String name;
+
+    public Genre(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return "Genre{" + "id=" + id + ", name='" + name + '\'' + '}';
+    }
+}
