59b0a765022aecba4fbb858e771e7387058d8fc5
tizzifona <tizzifona@gmail.com>
2025-05-29 12:44:53 +0200

    fix: added dotenv import and usage in DatabaseConnection, added DatabaseInitializer and method initialize in Main.java

--- src/main/java/org/LT3/Main.java
+++ src/main/java/org/LT3/Main.java
@@ -1,7 +1,9 @@
 package org.LT3;
 
+import org.LT3.config.DatabaseInitializer;
+
 public class Main {
     public static void main(String[] args) {
-
+        DatabaseInitializer.initialize();
     }
 }
