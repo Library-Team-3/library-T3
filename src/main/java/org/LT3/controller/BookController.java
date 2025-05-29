c149a91c03045b78125117ad960bfef068f5837e
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-05-29 16:04:03 +0200

    feat: BookController method getAllBooks

--- src/main/java/org/LT3/controller/BookController.java
+++ src/main/java/org/LT3/controller/BookController.java
@@ -1,4 +1,19 @@
 package org.LT3.controller;
 
+import org.LT3.model.Book;
+import org.LT3.repository.BookRepository;
+
+import java.util.List;
+
 public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
+    }
 }
