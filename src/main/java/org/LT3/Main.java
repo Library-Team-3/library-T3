9d41e99253c27261264f8b2206ba8ba6a3f7e7cc
tizzifona <tizzifona@gmail.com>
2025-05-30 12:12:02 +0200

    feat: added code for Main.java with initialisation of application

--- src/main/java/org/LT3/Main.java
+++ src/main/java/org/LT3/Main.java
@@ -1,9 +1,30 @@
 package org.LT3;
 
 import org.LT3.config.DatabaseInitializer;
+import org.LT3.controller.BookController;
+import org.LT3.repository.MySqlBookRepository;
+import org.LT3.view.BookView;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        DatabaseInitializer.initialize();
+        try {
+            DatabaseInitializer.initialize();
+
+            MySqlBookRepository bookRepository = new MySqlBookRepository();
+            BookController bookController = new BookController(bookRepository);
+            Scanner scanner = new Scanner(System.in);
+            BookView bookView = new BookView(bookController, scanner);
+
+            System.out.println("Welcome to the library!");
+            bookView.showMenu();
+
+            scanner.close();
+            System.out.println("Goodbye!");
+
+        } catch (Exception e) {
+            System.err.println("Error while starting the application: " + e.getMessage());
+        }
     }
 }
