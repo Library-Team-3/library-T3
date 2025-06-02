80422d8586e6de22dd0e054b27fb838972d75404
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-02 15:11:54 +0200

    feat: BookRepository add findById method

--- src/main/java/org/LT3/repository/BookRepository.java
+++ src/main/java/org/LT3/repository/BookRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface BookRepository {
     List<Book> findAll();
+    Book findById(Long id);
     void save(Book book);
     void update(Book book);
     void delete(Long id);
